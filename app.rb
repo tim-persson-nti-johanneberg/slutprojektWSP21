691c218b0c933b8b252b0cf757fc5062133f81ee
tim-persson-nti-johanneberg <tim.persson@elev.ga.ntig.se>
2021-03-05 15:03:15 +0100

    Påbörjaad admins sida
    Istället för loggbok.
    
    Började med admin sida. Lista som väljer alla genres från databasen och gör det möjligt att publicera en ny film på detta viset

--- app.rb
+++ app.rb
@@ -9,3 +9,18 @@ end
 get ('/edit') do
     slim(:edit)
 end
+
+get ('/admin') do
+    db = SQLite3::Database.new('db/reviews.db')
+    db.results_as_hash = true
+    genre = db.execute("SELECT * FROM GENRE")
+    slim(:admin, locals:{genre:genre})
+end
+
+post("/genre") do
+    db = SQLite3::Database.new('db/databas.db')
+    db.results_as_hash = true
+    genre_name = db.execute("SELECT genre_name FROM GENRE")
+    genre_id = db.execute("SELECT genre_id FROM GENRE")
+    slim(:admin, locals:{genre:genre_name, genre_id:genre_id})
+end
