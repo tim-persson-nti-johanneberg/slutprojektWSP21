73147942c04ac87530fdbf3a99f177f36d3a747d
tim-persson-nti-johanneberg <tim.persson@elev.ga.ntig.se>
2021-03-19 14:59:35 +0100

    Publiceras nu reviews tillsamans med movies

--- app.rb
+++ app.rb
@@ -23,10 +23,24 @@ post("/genre") do
 end
 
 post("/newreview") do
+    i = 0
     title = params[:Title]
     genre_chosen = params[:genre_chosen]
+    content = params[:CONTENT]
     genre_id = genre_chosen.to_i
     db = SQLite3::Database.new("db/reviews.db")
-    db.execute("INSERT INTO MOVIES (Title, Genre_id) VALUES(?,?)", title, genre_id)
+    titles_existing = db.execute("SELECT Title FROM MOVIES")
+    
+    titlesbool = titles_existing[0].include? title
+    byebug
+    if (titles_existing[0].include? title) == false
+        titdb.execute("INSERT INTO MOVIES (Title, Genre_id) VALUES(?,?)", title, genre_id) 
+    end
+
+    movie_id = db.execute("SELECT Movie_id FROM MOVIES WHERE Title = '#{title}'")
+    #
+    db.execute("INSERT INTO REVIEWS (review_text, movie_id) VALUES(?,?)", content, movie_id[0])
+    #Väljer index 0 för att movie_id blir en array pga att titlar inte blir unika
+    
     redirect('/edit')
 end
